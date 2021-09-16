b0dda3b34b4f0e18ba3815b7d079d2ef6b33d607
rafael.rezende <rafael.rezende@zup.com.br>
2021-09-16 11:03:44 -0300

    Preparando ambiente - Desafio Pix

---
+++ desafio-pix/src/test/kotlin/br/com/zup/DesafioPixTest.kt
@@ -0,0 +1,19 @@
+package br.com.zup
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class DesafioPixTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
